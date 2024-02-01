a7de303d3c0960780b545f8e8ec2c56c03319d7a
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-02-01 16:42:07 +0800

    20240201.164205

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM pudding/docker-app:node-18-7z-20230521
+# FROM node:21.6-bullseye
+
+RUN apt-get update --fix-missing
+
+COPY ./pandoc-3.1.11.1-1-amd64.deb /pandoc-3.1.11.1-1-amd64.deb
+
+RUN apt-get install -y /pandoc-3.1.11.1-1-amd64.deb
+RUN rm -y pandoc-3.1.11.1-1-amd64.deb
---
+++ app/index.js
@@ -0,0 +1,33 @@
+const ShellSpawn = require('./lib/ShellSpawn')
+const ShellExec = require('./lib/ShellExec')
+const GetFiles = require('./lib/GetFiles')
+
+const path = require('path')
+const fs = require('fs')
+
+// convert a.tif -thumbnail 64x64^ -gravity center -extent 64x64 b.ico
+
+let main = async function () {
+  let files = GetFiles()
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i]
+
+    // if (file.endsWith('.html') === false || file.endsWith('.htm') === false) {
+    //   continue
+    // }
+    
+    let filename = path.basename(file)
+    //let dirname = path.dirname(file)
+    let dirname = '/output/'
+    let filenameNoExt = filename
+    if (filenameNoExt.indexOf('.') > -1) {
+      filenameNoExt = filenameNoExt.slice(0, filenameNoExt.lastIndexOf('.'))
+    }
+
+    // https://stackoverflow.com/a/75518220
+    // pandoc --standalone --embed-resources --metadata pagetitle="title" index.md -o index-out.html
+    await ShellExec(`pandoc --standalone --embed-resources --metadata pagetitle="${filenameNoExt}" "${file}" -o "${path.resolve(dirname, filenameNoExt + '.docx')}"`)
+  }
+}
+
+main()
