8b9daf7f0b37127c01e655c412b2ffad6ac9e458
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-02-01 22:12:28 +0800

    西元2024年02月01日 (週四) 22時12分28秒 CST

--- app/cleanHTML.js
+++ app/cleanHTML.js
@@ -17,7 +17,7 @@ module.exports = function(inputFile) {
 
   $('img').each(function () {
     // $(this).css('max-width', '70%');
-    $(this).attr('width', '50%')
+    $(this).attr('width', '70%')
   });
 
   // =================================================================
